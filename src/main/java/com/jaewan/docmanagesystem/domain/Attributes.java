7189dcc43a3708113a39f25b1cdcc104fcfc0753
이재완 <jjjkuul@naver.com>
2025-09-11 19:51:01 +0900

    Attributes 수정

--- src/main/java/com/jaewan/docmanagesystem/domain/Attributes.java
+++ src/main/java/com/jaewan/docmanagesystem/domain/Attributes.java
@@ -4,19 +4,18 @@ public final class Attributes
 {
     public static final String PATH = "path";
     public static final String PATIENT = "patient";
-    public static final String PATIENT_PREFIX = "Patient: ";
     public static final String ADDRESS = "address";
     public static final String BODY = "body";
     public static final String WIDTH = "width";
     public static final String HEIGHT = "height";
     public static final String TYPE = "type";
     public static final String AMOUNT = "amount";
-    public static final String TEXT =  "text";
     public static final String DATE = "date";
     public static final String DRUG = "drug";
     public static final String DOSAGE= "dosage";
     public static final String INSTRUCTIONS = "instructions";
 
+    public static final String PATIENT_PREFIX = "Patient: ";
     public static final String NAME_PREFIX = "Dear ";
     public static final String AMOUNT_PREFIX = "Amount: ";
     public static final String DATE_PREFIX = "Date: ";
